e0caab85b87a12243d29b98ee06a0d8f48824d8e
unknown <ethan.sean.friedman@gmail.com>
2022-01-05 18:22:32 -0500

    Updates

---
+++ precache-manifest.b9323581b3bcf540596023ce6447f303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e803489eafaed0e2ee781ec140a0589",
+    "url": "/shishchat/index.html"
+  },
+  {
+    "revision": "c9c7ad104e7aa4ef26a7",
+    "url": "/shishchat/static/css/main.016158bf.chunk.css"
+  },
+  {
+    "revision": "86cb29dcb4d2c7a98b70",
+    "url": "/shishchat/static/js/2.dcc67109.chunk.js"
+  },
+  {
+    "revision": "bad4ae7a0c59b675b77d234ea1919dc7",
+    "url": "/shishchat/static/js/2.dcc67109.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9c7ad104e7aa4ef26a7",
+    "url": "/shishchat/static/js/main.6f0d1fe6.chunk.js"
+  },
+  {
+    "revision": "7c64388cc11b98248ee6",
+    "url": "/shishchat/static/js/runtime-main.6a17a8db.js"
+  }
+]);
